e6626afacdd91a14ddd6c47737c382db0d65a4c0
MattPua <matt.pua@utoronto.ca>
2013-08-31 02:03:42 -0400

    more details

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,21 +3,26 @@ class HomeController < ApplicationController
 		@description = "Hobbies & Interests"
 		@nextLink=experiences_path
 		@prevLink=root_path
+		@title = "Extra"
 	end
+
 
 	def about
 		@description = "About Me"
 		@nextLink=extra_path
+		@title="About"
 	end
 
 	def experiences
 		@description = "Experiences (XP)"
 		@nextLink=contact_path
 		@prevLink=extra_path
+		@title= "Experiences"
 	end
 
 	def contact
 		@description = "Call Me Maybe?"
 		@prevLink=experiences_path
+		@title="Contact"
 	end
 end
