a2895f3709fb17da69ccc25a6697ac4195336bf8
MattPua <matt.pua@utoronto.ca>
2013-08-31 01:29:01 -0400

    adding in experiences page basic layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,23 @@
 class HomeController < ApplicationController
 	def extra
 		@description = "Hobbies & Interests"
-		
+		@nextLink=experiences_path
+		@prevLink=root_path
 	end
 
 	def about
 		@description = "About Me"
+		@nextLink=extra_path
 	end
 
 	def experiences
-		@description = "Experience (XP)"
+		@description = "Experiences (XP)"
+		@nextLink=contact_path
+		@prevLink=extra_path
 	end
 
 	def contact
 		@description = "Call Me Maybe?"
+		@prevLink=experiences_path
 	end
 end
